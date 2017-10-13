7f1025c07711a0d0b88ff0170fa6f8ba86ac2a4c
Dhruv Hegde <desidhruv@gmail.com>
2017-10-13 21:55:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,32 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-array[0] = element;
-return array;
+array.unshift(element); 
+return array; 
+}
+function addElementToEndOfArray(array, element){ 
+return [...array, element]; 
+}
+function destructivelyAddElementToEndOfArray(array, element){ 
+array.push(element); 
+return array; 
+}
+function accessElementInArray(array, index){ 
+return array[index]; 
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){ 
+array.shift(); 
+return array; 
+}
+function removeElementFromBeginningOfArray(array){ 
+array.slice(); 
+return array; 
+}
+function destructivelyRemoveElementFromEndOfArray(array){ 
+array.pop(); 
+return array; 
+}
+function removeElementFromEndOfArray(array){ 
+array.slice(array.length-1); 
+return array; 
 }
